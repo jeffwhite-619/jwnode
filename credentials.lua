aee18e76432bca0e7057d23d4f0397f9e7bf0c82
Jeff White <thejeffwhite@gmail.com>
2018-11-07 19:43:56 -0800

    MQTT broker credentials added

--- credentials.lua
+++ credentials.lua
@@ -1,3 +1,5 @@
+-- credentials.lua
+
 SSID = ""
 PASSWORD = ""
 CLIENTID = "ESP8266-"..node.chipid()
@@ -5,3 +7,10 @@ IPADR = "192.168.1.15" -- Requested static IP address for the ESP
 IPROUTER = "192.168.0.1" -- IP address for the Wifi router
 NODEIPADR = "192.168.1.5" -- private IP for node's wifi AP
 NODEIPROUTER = "192.168.1.1"
+
+-- values for MQTT broker
+BROKER = "BROKER" -- BROKER SERVER (e.g., z99.cloudmqtt.com)
+BUSER = "BROKER USER"
+BRPORT = 11111 -- BROKER PORT
+SSLPORT = 22222
+BRPWD = "BROKER PASSWORD"
