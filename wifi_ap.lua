9cd2065015493c440ee926759adf49c04f21c339
Jeff White <thejeffwhite@gmail.com>
2019-02-07 22:34:07 -0800

    Resolved some errors where callbacks attempted to print tables

--- wifi_ap.lua
+++ wifi_ap.lua
@@ -1,7 +1,6 @@
 -- ap_cfg.lua
 dofile("credentials.lua")
 
-
 print("Starting wifi access point...")
 
 ap_cfg={}
@@ -10,20 +9,29 @@ ap_cfg.pwd="!1@2#3$4%5^6&7*8(9)0"
 ap_cfg.auth=wifi.WPA_WPA2_PSK
 ap_cfg.save=true
 ap_cfg.staconnected_cb=function(mac,aid)
-    print("AP Client connected. MAC: "..mac.." | AID: "..aid.."\r\n")
+    print("AP Client connected\r\n")
+    if (aid) then
+        print("AID: "..aid.."\r\n")
+    end
 end
 ap_cfg.stadisconnected_cb=function(mac,aid)
-    print("AP Client disconnected. MAC: "..mac.." | AID: "..aid.."\r\n")
+    print("AP Client disconnected\r\n")
+    if (aid) then
+        print("AID: "..aid.."\r\n")
+    end
 end
 ap_cfg.probereq_cb=function(mac,rssi)
-    print("AP Client probing... MAC: "..mac.." | RSSI (Received Signal Strength Ind.) : "..rssi.."\r\n")
+    if (rssi) then
+        print("RSSI (Received Signal Strength Ind.) : "..rssi.."\r\n")
+    end
+    -- print("AP Client probing... MAC: "..mac.." | RSSI (Received Signal Strength Ind.) : "..rssi.."\r\n")
 end
 
 wifi.ap.config(ap_cfg)
 wifi.ap.setip({ip=NODEIPADR,netmask="255.255.255.0",gateway=NODEIPROUTER})
 
 dhcp_config ={}
-dhcp_config.start = "192.168.1.100"
+dhcp_config.start = NODEIPROUTER
 wifi.ap.dhcp.config(dhcp_config)
 wifi.ap.dhcp.start()
 
